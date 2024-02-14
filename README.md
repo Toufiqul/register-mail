b524e1e7cdaba8d09c4cd1343861336b42114cbb
Toufiqul <fahim.prime@gmail.com>
2024-02-14 17:48:11 +0600

    feat: runnable with docker-compose up --build

--- README.md
+++ README.md
@@ -2,7 +2,24 @@ This project has one API endpoint at "/register".
 This endpoint takes the user's email address and saves it to a database, returns a JSON response and sends the user a confirmation email.
 
 Tech stack:
+
 - Flask
 - SQLite
 - SQLAlchemy
 - Gmail API
+
+#### to run
+
+- clone the repository https://github.com/Toufiqul/register-mail.git
+- build docker image
+
+```
+docker build -t register-mail .
+```
+
+- run docker container
+
+```
+docker run -p 5000:5000 -w /app -v "$(pwd):/app" register-mail
+
+```
--- app.py
+++ app.py
@@ -1,8 +1,41 @@
 from flask import Flask, request, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db import db
+from models import UserModel
 
+# def create_app():
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///data.db"
+db.init_app(app)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+with app.app_context():
+    db.create_all()
+
+
 @app.post('/register')
-def hello_world():
+def reg():
     data = request.get_json()
-    return jsonify(data)
+    # return f"{data['id']}, {data['email']},{data['username']}"
+    # nUser = {
+    #     "username": data["username"],
+    #     "mail": data["mail"],
+    #     "id": data["id"] 
+    # }
+    try:
+        newUser = UserModel(id=data['id'], email=data['email'],username=data['username'],)
+
+        db.session.add(newUser)
+        db.session.commit()
+        return jsonify(newUser)
+    except Exception as e:
+        print(e)
+        return jsonify(e)
+
+@app.get('/users')
+def get_users():
+    return "not implemented "
+    # return UserModel.query.all()
+    # return app
