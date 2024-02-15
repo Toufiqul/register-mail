a28afd95e7a77cff999a6c27dbe8b95432ad1ad6
Toufiqul <fahim.prime@gmail.com>
2024-02-15 09:57:12 +0600

    feat: hotreload. add 'flask-dotenv' to requirements file and FLASK_DEBUG=True in .flaskenv file

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 with app.app_context():
     db.create_all()
 
-
+# def create_app()
 @app.post('/register')
 def reg():
     data = request.get_json()
